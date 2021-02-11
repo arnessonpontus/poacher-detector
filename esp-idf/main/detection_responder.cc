661d360f84ddc304fbc367136c246cea8b82eae0
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-02-11 15:27:22 +0100

    Sending converted jpeg images to server
    Co-authored-by: Johan Forslund <johanforslund95@gmail.com>

--- esp-idf/main/detection_responder.cc
+++ esp-idf/main/detection_responder.cc
@@ -18,13 +18,18 @@ limitations under the License.
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "esp_log.h"
+#include "esp_websocket_client.h"
+#include "esp_event.h"
+
 // This dummy implementation writes person and no person scores to the error
 // console. Real applications will want to take some custom action instead, and
 // should implement their own versions of this function.
 void RespondToDetection(tflite::ErrorReporter *error_reporter,
                         uint8_t person_score, uint8_t no_person_score)
 {
-  if (person_score > no_person_score)
+
+  if (person_score > no_person_score && person_score > 200)
   {
     gpio_reset_pin((gpio_num_t)4);
     gpio_set_direction((gpio_num_t)4, GPIO_MODE_OUTPUT);
