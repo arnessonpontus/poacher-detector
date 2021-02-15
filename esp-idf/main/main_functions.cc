6a3dad2214a4ce8b2df84d870ea6639b127be371
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-02-15 15:01:48 +0100

    Cleanup in main_functions.cc
    Co-authored-by: Johan Forslund <johanforslund95@gmail.com>

--- esp-idf/main/main_functions.cc
+++ esp-idf/main/main_functions.cc
@@ -33,25 +33,19 @@ limitations under the License.
 #include "esp_event.h"
 #include "protocol_examples_common.h"
 #include "esp_websocket_client.h"
-#include "esp_event.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "freertos/event_groups.h"
 
 #include "esp_log.h"
-#include "esp_websocket_client.h"
-#include "esp_event.h"
 
 #include "img_converters.h"
-#include "sdmmc_cmd.h"
-#include "sdkconfig.h"
 
 #include <string.h>
 #include <sys/unistd.h>
 #include <sys/stat.h>
 #include "esp_err.h"
-#include "esp_log.h"
 #include "esp_vfs_fat.h"
 #include "driver/sdspi_host.h"
 #include "driver/spi_common.h"
@@ -64,42 +58,10 @@ limitations under the License.
 
 #define MOUNT_POINT "/sdcard"
 
-// ESP32-S2 and ESP32-C3 doesn't have an SD Host peripheral, always use SPI:
-#if CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32C3
-#ifndef USE_SPI_MODE
-#define USE_SPI_MODE
-#endif // USE_SPI_MODE
-// on ESP32-S2, DMA channel must be the same as host id
-#define SPI_DMA_CHAN host.slot
-#endif //CONFIG_IDF_TARGET_ESP32S2
-
 // DMA channel to be used by the SPI peripheral
 #ifndef SPI_DMA_CHAN
 #define SPI_DMA_CHAN 1
 #endif //SPI_DMA_CHAN
-
-// When testing SD and SPI modes, keep in mind that once the card has been
-// initialized in SPI mode, it can not be reinitialized in SD mode without
-// toggling power to the card.
-
-#ifdef USE_SPI_MODE
-// Pin mapping when using SPI mode.
-// With this mapping, SD card can be used both in SPI and 1-line SD mode.
-// Note that a pull-up on CS line is required in SD mode.
-#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-#define PIN_NUM_MISO 2
-#define PIN_NUM_MOSI 15
-#define PIN_NUM_CLK 14
-#define PIN_NUM_CS 13
-
-#elif CONFIG_IDF_TARGET_ESP32C3
-#define PIN_NUM_MISO 18
-#define PIN_NUM_MOSI 9
-#define PIN_NUM_CLK 8
-#define PIN_NUM_CS 19
-
-#endif //CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
-#endif //USE_SPI_MODE
 
 #define W 96
 #define H 96
@@ -152,102 +114,17 @@ static void websocket_app_start(void)
   esp_websocket_client_start(client);
 }
 
-// The name of this function is important for Arduino compatibility.
-void setup()
+void setup_sdcard()
 {
-  // Setup wifi and ws
-  ESP_LOGI(TAG, "[APP] Startup..");
-  ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-  esp_log_level_set("*", ESP_LOG_INFO);
-  esp_log_level_set("WEBSOCKET_CLIENT", ESP_LOG_DEBUG);
-  esp_log_level_set("TRANSPORT_WS", ESP_LOG_DEBUG);
-  esp_log_level_set("TRANS_TCP", ESP_LOG_DEBUG);
-
-  ESP_ERROR_CHECK(nvs_flash_init());
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-  /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-  ESP_ERROR_CHECK(example_connect());
-
-  websocket_app_start();
-
-  // Set up logging. Google style is to avoid globals or statics because of
-  // lifetime uncertainty, but since this has a trivial destructor it's okay.
-  // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::MicroErrorReporter micro_error_reporter;
-  error_reporter = &micro_error_reporter;
-
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  model = tflite::GetModel(g_person_detect_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION)
-  {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Model provided is schema version %d not equal "
-                         "to supported version %d.",
-                         model->version(), TFLITE_SCHEMA_VERSION);
-    return;
-  }
-
-  // Pull in only the operation implementations we need.
-  // This relies on a complete list of all the ops needed by this graph.
-  // An easier approach is to just use the AllOpsResolver, but this will
-  // incur some penalty in code space for op implementations that are not
-  // needed by this graph.
-  //
-  // tflite::AllOpsResolver resolver;
-  // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::MicroMutableOpResolver<3> micro_op_resolver;
-  micro_op_resolver.AddBuiltin(
-      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-                               tflite::ops::micro::Register_CONV_2D());
-  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_AVERAGE_POOL_2D,
-                               tflite::ops::micro::Register_AVERAGE_POOL_2D());
-
-  // Build an interpreter to run the model with.
-  static tflite::MicroInterpreter static_interpreter(
-      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
-  interpreter = &static_interpreter;
-
-  // Allocate memory from the tensor_arena for the model's tensors.
-  TfLiteStatus allocate_status = interpreter->AllocateTensors();
-  if (allocate_status != kTfLiteOk)
-  {
-    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
-    return;
-  }
-
-  // Get information about the memory area to use for the model's input.
-  input = interpreter->input(0);
-
-  // Initialize SD card
   esp_err_t ret;
-  // Options for mounting the filesystem.
-  // If format_if_mount_failed is set to true, SD card will be partitioned and
-  // formatted in case when mounting fails.
   esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-#ifdef CONFIG_EXAMPLE_FORMAT_IF_MOUNT_FAILED
-      .format_if_mount_failed = true,
-#else
       .format_if_mount_failed = false,
-#endif // EXAMPLE_FORMAT_IF_MOUNT_FAILED
       .max_files = 5,
       .allocation_unit_size = 16 * 1024};
   sdmmc_card_t *card;
   const char mount_point[] = MOUNT_POINT;
   ESP_LOGI(TAG, "Initializing SD card");
 
-  // Use settings defined above to initialize SD card and mount FAT filesystem.
-  // Note: esp_vfs_fat_sdmmc/sdspi_mount is all-in-one convenience functions.
-  // Please check its source code and implement error recovery when developing
-  // production applications.
   ESP_LOGI(TAG, "Using SDMMC peripheral");
   sdmmc_host_t host = SDMMC_HOST_DEFAULT();
 
@@ -258,9 +135,6 @@ void setup()
   // To use 1-line SD mode, uncomment the following line:
   slot_config.width = 1;
 
-  // GPIOs 15, 2, 4, 12, 13 should have external 10k pull-ups.
-  // Internal pull-ups are not sufficient. However, enabling internal pull-ups
-  // does make a difference some boards, so we do that here.
   gpio_set_pull_mode((gpio_num_t)15, GPIO_PULLUP_ONLY); // CMD, needed in 4- and 1- line modes
   gpio_set_pull_mode((gpio_num_t)2, GPIO_PULLUP_ONLY);  // D0, needed in 4- and 1-line modes
   gpio_set_pull_mode((gpio_num_t)4, GPIO_PULLUP_ONLY);  // D1, needed in 4-line mode only
@@ -287,6 +161,72 @@ void setup()
 
   // Card has been initialized, print its properties
   sdmmc_card_print_info(stdout, card);
+}
+
+void setup()
+{
+  // Setup wifi and ws
+  ESP_LOGI(TAG, "[APP] Startup..");
+  ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+  esp_log_level_set("*", ESP_LOG_INFO);
+  esp_log_level_set("WEBSOCKET_CLIENT", ESP_LOG_DEBUG);
+  esp_log_level_set("TRANSPORT_WS", ESP_LOG_DEBUG);
+  esp_log_level_set("TRANS_TCP", ESP_LOG_DEBUG);
+
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+  ESP_ERROR_CHECK(example_connect());
+
+  websocket_app_start();
+
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = tflite::GetModel(g_person_detect_model_data);
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  static tflite::MicroMutableOpResolver<3> micro_op_resolver;
+  micro_op_resolver.AddBuiltin(
+      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
+      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
+                               tflite::ops::micro::Register_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_AVERAGE_POOL_2D,
+                               tflite::ops::micro::Register_AVERAGE_POOL_2D());
+
+  // Build an interpreter to run the model with.
+  static tflite::MicroInterpreter static_interpreter(
+      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk)
+  {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
+
+  // Get information about the memory area to use for the model's input.
+  input = interpreter->input(0);
+
+  setup_sdcard();
 }
 
 // The name of this function is important for Arduino compatibility.
@@ -329,9 +269,6 @@ void loop()
     {
       esp_websocket_client_send(client, (const char *)jpeg, len, portMAX_DELAY);
     }
-
-    // Use POSIX and C standard library functions to work with files.
-    // First create a file.
 
     ESP_LOGI(TAG, "Opening file");
     char buf[0x100];
