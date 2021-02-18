fd211d9198a89e3072c1f3b6e36b589be59d72e6
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-02-18 10:50:27 +0100

    Gray scale motion detection bg. sub and draw blob bbox script
    Co-authored-by: Johan Forslund <johanforslund95@gmail.com>

--- pipeline/client/client.ino
+++ pipeline/client/client.ino
@@ -1,248 +1,208 @@
 #define CAMERA_MODEL_AI_THINKER
 
-#include <ArduinoWebsockets.h>
+#include "esp_camera.h"
+#include "camera_pins.h"
 
 #include <FS.h>
 #include "SD_MMC.h"
-#include <EloquentArduino.h>
-#include <eloquentarduino/io/serial_print.h>
-#include <eloquentarduino/vision/camera/ESP32Camera.h>
-#include <eloquentarduino/vision/io/writers/JpegWriter.h>
-#include <Preferences.h>
 
-#define FRAME_SIZE FRAMESIZE_VGA
-#define PIXFORMAT PIXFORMAT_RGB565
-#define W 640
-#define H 480
-#define w 64
-#define h 48
-#define DIFF_THRESHOLD 15
-#define MOTION_THRESHOLD 0.05
-#define FLASH_LENGTH 500
-#define LED_PIN 33
-#define ADOPTION_RATE 1.0
-#define THRESH 80
+#define FRAME_SIZE FRAMESIZE_QVGA
+#define WIDTH 320
+#define HEIGHT 240
+#define BLOCK_SIZE 10
+#define W (WIDTH / BLOCK_SIZE)
+#define H (HEIGHT / BLOCK_SIZE)
+#define BLOCK_DIFF_THRESHOLD 0.2
+#define IMAGE_DIFF_THRESHOLD 0.1
 
-// delete the second definition if you want to turn on code benchmarking
-#define timeit(label, code)                                               \
-  {                                                                       \
-    uint32_t start = millis();                                            \
-    code;                                                                 \
-    uint32_t duration = millis() - start;                                 \
-    eloquent::io::print_all("It took ", duration, " millis for ", label); \
-  }
-//#define timeit(label, code) code;
+uint16_t prev_frame[H][W] = { 0 };
+uint16_t current_frame[H][W] = { 0 };
 
-const char *ssid = "PonePlus";
-const char *password = "Sutnop123";
-const char *websocket_server_host = "192.168.160.93";
-const uint16_t websocket_server_port = 8888;
+uint16_t pictureNumber = 0;
 
-using namespace websockets;
-using namespace Eloquent::Vision;
+bool setup_camera(framesize_t);
+bool capture_still();
+bool motion_detect();
+void update_frame();
 
-WebsocketsClient client;
 
-camera_fb_t *frame;
-Camera::ESP32Camera camera(PIXFORMAT);
-uint8_t downscaled[w * h];
-//IO::Decoders::GrayscaleRandomAccessDecoder decoder;
-IO::Decoders::Red565RandomAccessDecoder decoder;
-Processing::Downscaling::Center<W / w, H / h> strategy;
-Processing::Downscaling::Downscaler<W, H, w, h> downscaler(&decoder, &strategy);
-Processing::MotionDetector<w, h> motion;
-unsigned int pictureNumber = 0;
-unsigned int gray_number = 0;
-unsigned long triggered_ms = 0;
-Preferences preferences;
-uint8_t *median;
-bool is_first_frame = true;
+/**
+ *
+ */
+void setup() {
+    Serial.begin(115200);
+    Serial.println(setup_camera(FRAME_SIZE) ? "OK" : "ERR INIT");
 
-void capture();
-void save();
-void stream_downscaled();
-void stream();
-void setup_connection();
-
-void setup()
-{
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("Begin");
-
-  median = (uint8_t *)heap_caps_malloc(W * H, MALLOC_CAP_SPIRAM);
-
-  preferences.begin("poach_det", false);
-  pictureNumber = preferences.getUInt("camera_counter", 0);
-
-  setup_connection();
-
-  pinMode(LED_PIN, OUTPUT); // Set led pin
-
-  if (!SD_MMC.begin("/sdcard", true))
-  {
-    Serial.println("SD Card Mount Failed");
-    return;
-  }
-
-  uint8_t cardType = SD_MMC.cardType();
-  if (cardType == CARD_NONE)
-  {
-    Serial.println("No SD Card attached");
-    return;
-  }
-
-  camera.begin(FRAME_SIZE, 30, 10000000);
+    if (!SD_MMC.begin("/sdcard", true))
+    {
+      Serial.println("SD Card Mount Failed");
+      return;
+    }
+  
+    uint8_t cardType = SD_MMC.cardType();
+    if (cardType == CARD_NONE)
+    {
+      Serial.println("No SD Card attached");
+      return;
+    }
 }
 
-void loop()
-{
-  unsigned long current_ms = millis();
-  Serial.println(heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+/**
+ *
+ */
+void loop() {
+    if (!capture_still()) {
+        Serial.println("Failed capture");
+        delay(3000);
 
-  capture();
+        return;
+    }
 
-  esp_camera_fb_return(frame);
-  delay(30);
+    if (motion_detect()) {
+        Serial.println("Motion detected");
+    }
+
+    update_frame();
+    Serial.println("=================");
 }
 
-void setup_connection()
-{
-  WiFi.begin(ssid, password);
 
-  uint8_t connection_attempts = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
+/**
+ *
+ */
+bool setup_camera(framesize_t frameSize) {
+    camera_config_t config;
 
-    if (connection_attempts > 2)
-    {
-      ESP.restart();
-    }
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sscb_sda = SIOD_GPIO_NUM;
+    config.pin_sscb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.pixel_format = PIXFORMAT_GRAYSCALE;
+    config.frame_size = frameSize;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
 
-    connection_attempts++;
-  }
+    bool ok = esp_camera_init(&config) == ESP_OK;
 
-  Serial.println("");
-  Serial.println("WiFi connected");
+    sensor_t *sensor = esp_camera_sensor_get();
+    sensor->set_framesize(sensor, frameSize);
 
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-
-  while (!client.connect(websocket_server_host, websocket_server_port, "/"))
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Websocket Connected!");
+    return ok;
 }
 
-void capture()
-{
-  timeit("capture frame", frame = camera.capture());
+/**
+ * Capture image and do down-sampling
+ */
+bool capture_still() {
+    camera_fb_t *frame_buffer = esp_camera_fb_get();
 
-  // Remove first bad images
-  if (gray_number < 2)
-  {
-    gray_number++;
-    return;
-  }
+    if (!frame_buffer)
+        return false;
 
-  uint32_t ARRAY_LENGTH = frame->width * frame->height;
+    // set all 0s in current frame
+    for (int y = 0; y < H; y++)
+        for (int x = 0; x < W; x++)
+            current_frame[y][x] = 0;
 
-  uint8_t *gray_image = (uint8_t *)heap_caps_malloc(ARRAY_LENGTH, MALLOC_CAP_SPIRAM);
-  char *bg_image_str = (char *)heap_caps_malloc(ARRAY_LENGTH + 1, MALLOC_CAP_SPIRAM);
 
-  for (int i = 0; i < H; i++)
-  {
-    for (int j = 0; j < W; j++)
-    {
-      gray_image[i * W + j] = decoder.get(frame->buf, W, H, j, i);
-    }
-  }
+    // down-sample image in blocks
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; i++) {
+        const uint16_t x = i % WIDTH;
+        const uint16_t y = floor(i / WIDTH);
+        const uint8_t block_x = floor(x / BLOCK_SIZE);
+        const uint8_t block_y = floor(y / BLOCK_SIZE);
+        const uint8_t pixel = frame_buffer->buf[i];
+        const uint16_t current = current_frame[block_y][block_x];
 
-  if (is_first_frame == true)
-  {
-    is_first_frame = false;
-
-    //memcpy(median, gray_image, ARRAY_LENGTH * sizeof(uint8_t));
-    heap_caps_free(gray_image);
-    heap_caps_free(bg_image_str);
-    return;
-  }
-
-  for (int i = 0; i < ARRAY_LENGTH; i++)
-  {
-    /*
-    if (gray_image[i] > median[i]) {
-      median[i] += ADOPTION_RATE;
-    } else {
-      median[i] -= ADOPTION_RATE;
-    }
-    
-
-    if (abs(gray_image[i] - median[i]) <= THRESH) {
-      gray_image[i] = 2;
-    } else {
-      gray_image[i] = 1;
-    }*/
-    gray_image[i] = 1;
-  }
-
-  if (gray_number > 3)
-  {
-    for (int i = 0; i < ARRAY_LENGTH; i++)
-    {
-      bg_image_str[i] = gray_image[i] + '0';
+        // average pixels in block (accumulate)
+        current_frame[block_y][block_x] += pixel;
     }
 
-    bg_image_str[ARRAY_LENGTH] = '\0';
+    // average pixels in block (rescale)
+    for (int y = 0; y < H; y++)
+        for (int x = 0; x < W; x++)
+            current_frame[y][x] /= BLOCK_SIZE * BLOCK_SIZE;
+
+    return true;
+}
+
+
+/**
+ * Compute the number of different blocks
+ * If there are enough, then motion happened
+ */
+bool motion_detect() {
+    uint16_t changes = 0;
+    const uint16_t blocks = (WIDTH * HEIGHT) / (BLOCK_SIZE * BLOCK_SIZE);
+
+    char *bg_image_str = (char *)heap_caps_malloc(W*H + 1, MALLOC_CAP_SPIRAM);
+
+    for (int y = 0; y < H; y++) {
+        for (int x = 0; x < W; x++) {
+            float current = current_frame[y][x];
+            float prev = prev_frame[y][x];
+            float delta = abs(current - prev) / prev;
+
+            if (delta >= BLOCK_DIFF_THRESHOLD) {
+                changes += 1;
+
+                bg_image_str[x + y*W] = '0';
+            } else {
+                bg_image_str[x + y*W] = '1';
+            }
+            
+        }
+    }
+
+    bg_image_str[W*H] = '\0';
 
     fs::FS &fs = SD_MMC;
-    String path = "/esp/gray" + String(gray_number) + ".txt";
-
-    File file = fs.open(path.c_str(), FILE_WRITE);
+    char buf[0x100];
+    snprintf(buf, sizeof(buf), "/esp/images/esp_picture%04d.txt", ++pictureNumber);
+  
+    File file = fs.open(buf, FILE_WRITE);
     if (!file)
     {
       Serial.println("Failed to open file in writing mode");
     }
-
-    Serial.println("Writing to file " + path);
-
+  
+    Serial.println("Writing to file " + (String)buf);
+  
     file.println(bg_image_str);
-
-    /*
-    for (int i = 0; i < ARRAY_LENGTH; i++) {
-      file.println(gray_image[i]);
-    }
-    */
-
+  
     file.close();
-  }
 
-  gray_number++;
+    Serial.print("Changed ");
+    Serial.print(changes);
+    Serial.print(" out of ");
+    Serial.println(blocks);
 
-  heap_caps_free(gray_image);
-  heap_caps_free(bg_image_str);
+    return (1.0 * changes / blocks) > IMAGE_DIFF_THRESHOLD;
 }
 
-void save(uint8_t *jpeg, size_t len)
-{
-  fs::FS &fs = SD_MMC;
-  String path = "/esp/images/esp_picture" + String(pictureNumber) + ".jpg";
 
-  File file = fs.open(path.c_str(), FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file in writing mode");
-  }
-  else
-  {
-    file.write(jpeg, len);
-    Serial.printf("Saved file to path: %s\n", path.c_str());
-    preferences.putUInt("camera_counter", ++pictureNumber);
-  }
-  file.close();
+/**
+ * Copy current frame to previous
+ */
+void update_frame() {
+    for (int y = 0; y < H; y++) {
+        for (int x = 0; x < W; x++) {
+            prev_frame[y][x] = current_frame[y][x];
+        }
+    }
 }
