c7f3435c975683c422cb6f291efbe51c56584be5
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-03-01 14:45:42 +0100

    Refactored main_functions into multiple files and started evaluate file
    Co-authored-by: Johan Forslund <johanforslund95@gmail.com>

--- esp-idf/main/main_functions.h
+++ esp-idf/main/main_functions.h
@@ -12,9 +12,57 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-
 #ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_MAIN_FUNCTIONS_H_
 #define TENSORFLOW_LITE_MICRO_EXAMPLES_PERSON_DETECTION_MAIN_FUNCTIONS_H_
+
+#include <sys/time.h>
+#include "image_util.h"
+// #define STB_IMAGE_RESIZE_IMPLEMENTATION
+// #include "stb_image_resize.h"
+// #define STBIR_ASSERT()
+#include "image_provider.h"
+
+#include <stdio.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "protocol_examples_common.h"
+#include "esp_websocket_client.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+
+#include "esp_log.h"
+
+#include "img_converters.h"
+
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdspi_host.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#include "sdkconfig.h"
+#include "preferences.h"
+#include <errno.h>
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+#include "driver/sdmmc_host.h"
+#endif
+
+#define timeit(label, code)                                                                                            \
+{                                                                                                                      \
+  struct timeval stop, start;                                                                                          \
+  gettimeofday(&start, NULL);                                                                                          \
+  code;                                                                                                                \
+  gettimeofday(&stop, NULL);                                                                                           \
+  ESP_LOGI(TAG, "%s took %f us", label, (float)(stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); \
+}
+//#define timeit(label, code) code;
 
 // Expose a C friendly interface for main functions.
 #ifdef __cplusplus
@@ -25,10 +73,21 @@ extern "C" {
 // to be setup() for Arduino compatibility.
 void setup();
 
+
 // Runs one iteration of data gathering and inference. This should be called
 // repeatedly from the application code. The name needs to be loop() for Arduino
 // compatibility.
 void loop();
+
+void crop_image_center(uint8_t *src, uint8_t *dst);
+void crop_image(uint8_t *src, uint8_t *dst, uint16_t changes, uint32_t &cropped_len, uint16_t &accumelated_x, uint16_t &accumelated_y);
+void bg_subtraction(uint16_t &changes, uint16_t &accumelated_x, uint16_t &accumelated_y);
+void update_frame();
+bool downscale(uint8_t *image);
+void setup_sdcard();
+void save_to_sdcard(uint8_t *image, size_t len);
+void get_stored_image(uint8_t* input_image);
+void setup_mf();
 
 #ifdef __cplusplus
 }
