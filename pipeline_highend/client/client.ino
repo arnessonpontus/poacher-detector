ecf7a26df7151a11f2391737c33cb9b76a0e8c75
Pontus Arnesson <arnesson.pontus@gmail.com>
2021-03-29 16:05:34 +0200

    Send from ESP to high-end and run inference
    Co-authored-by: Johan Forslund <johanforslund95@gmail.com>

---
+++ pipeline_highend/client/client.ino
@@ -0,0 +1,116 @@
+#include <ArduinoWebsockets.h>
+
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#include "camera_pins.h"
+
+const char* ssid = "Pone Plus";
+const char* password = "Sutnop123";
+const char* websocket_server_host = "192.168.243.93";
+const uint16_t websocket_server_port = 8888;
+
+using namespace websockets;
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connnection Opened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("Connnection Closed");
+        esp_restart();
+    } else if(event == WebsocketsEvent::GotPing) {
+        Serial.println("Got a Ping!");
+    } else if(event == WebsocketsEvent::GotPong) {
+        Serial.println("Got a Pong!");
+    }
+}
+
+WebsocketsClient client;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 40;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
+  client.onEvent(onEventsCallback);
+
+  while (!client.connect(websocket_server_host, websocket_server_port, "/"))
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Websocket Connected!");
+  
+  Serial.println("Websocket Hopefully Connected!");
+}
+
+void loop() {
+  camera_fb_t *fb = esp_camera_fb_get();
+  if(!fb){
+    Serial.println("Camera capture failed");
+    esp_camera_fb_return(fb);
+    return;
+  }
+
+  if(fb->format != PIXFORMAT_JPEG){
+    Serial.println("Non-JPEG data not implemented");
+    return;
+  }
+  Serial.println("Image sent!");
+  client.sendBinary((const char*) fb->buf, fb->len);
+
+  delay(300);
+  esp_camera_fb_return(fb);
+}
---
+++ pipeline_highend/server/app.py
@@ -0,0 +1,130 @@
+import asyncio
+import websockets
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import time
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')   # Suppress Matplotlib warnings
+import random
+import logging
+logger = logging.getLogger('websockets.server')
+logger.setLevel(logging.ERROR)
+logger.addHandler(logging.StreamHandler())
+import cv2
+
+detect_fn = None
+category_index = None
+
+def load_image_into_numpy_array(path):
+    """Load an image from file into a numpy array.
+
+    Puts image into numpy array to feed into tensorflow graph.
+    Note that by convention we put it into a numpy array with shape
+    (height, width, channels), where channels=3 for RGB.
+
+    Args:
+      path: the file path to the image
+
+    Returns:
+      uint8 numpy array with shape (img_height, img_width, 3)
+    """
+    return np.array(Image.open(path))
+
+def run_inference(message):
+    image_path = "../../dataset/test/image_{0:04}.jpg".format(random.randint(0, 101))
+    #print('Running inference for {}... '.format(image_path), end='')
+
+    file_jpgdata = BytesIO(message)
+    img = Image.open(file_jpgdata)
+    image_np = np.array(img)
+
+    #image_np = load_image_into_numpy_array(image_path)
+
+    # Things to try:
+    # Flip horizontally
+    # image_np = np.fliplr(image_np).copy()
+
+    # Convert image to grayscale
+    # image_np = np.tile(
+    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detect_fn(input_tensor)
+
+    max_score = np.max(detections['detection_scores'])
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+
+    detections = {key: value[0, :num_detections].numpy()
+                    for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'],
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=200,
+            min_score_thresh=.30,
+            agnostic_mode=False)
+
+    image = Image.fromarray(image_np_with_detections)
+    #buf = BytesIO()
+    #image.save(buf, format="JPEG")
+
+    return image, max_score
+
+async def hello(websocket, path):
+    while True:
+        async for message in websocket:
+            image, max_score = run_inference(message)
+            cv2.imshow('image', np.array(image))
+            cv2.waitKey(1)
+            
+            print("Max score: ", max_score)
+            if max_score > 0.3:
+                print("*********HUMAN DETECTED**********")
+        #await websocket.send(message)
+
+if __name__ == '__main__':
+    PATH_TO_SAVED_MODEL = "../local_inference/model/saved_model"
+    PATH_TO_LABELS = "../local_inference/label_map.pbtxt"
+
+    print('Loading model...', end='')
+    start_time = time.time()
+
+    # Load saved model and build the detection function
+    detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('Done! Took {} seconds'.format(elapsed_time))
+
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS,
+                                                                    use_display_name=True)
+
+    asyncio.get_event_loop().run_until_complete(
+        websockets.serve(hello, '192.168.243.93', 8888, ping_interval=None))
+    asyncio.get_event_loop().run_forever()
